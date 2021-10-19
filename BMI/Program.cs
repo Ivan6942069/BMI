76ffc489bc50fe5c48a337bbf6503d8d5e7aa552
sorban.ivan <sorban.ivan@D20-06>
2021-10-19 11:28:09 +0200

    dodelany

--- BMI/Program.cs
+++ BMI/Program.cs
@@ -6,7 +6,42 @@ namespace BMI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double vaha;
+            double vyska;
+            double vypocet;
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("vypocet BMI");
+                Console.WriteLine("-----------------");
+
+                Console.Write("zadej vahu:");
+                double.TryParse(Console.ReadLine(), out vaha);
+
+                if (vaha <= 30)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("jenom cisla nad 30 plati");
+                }
+                else
+                {
+                    Console.Write("zadej vysku (cm): ");
+                    double.TryParse(Console.ReadLine(), out vyska);
+                    if (vyska <= 100)
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("jenom cisla nad 100 plati");
+                    }
+                    else
+                    {
+                        vypocet = Math.Pow(vyska / 100, 2);
+                        vypocet = vaha / (vypocet);
+                        Console.WriteLine();
+                        Console.Write($"Tvoje BMI je {vypocet}.");
+                    }
+                }
+                Console.ReadLine();
+            }
         }
     }
 }
