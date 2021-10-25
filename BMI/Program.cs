0fc0107c5d97f87609874abfb0c06848c1e14eb1
Ivan Šorban <Sorban.Ivan@stud.vassboskovice.cz>
2021-10-25 20:23:52 +0200

    dokonceno uplne

--- BMI/Program.cs
+++ BMI/Program.cs
@@ -38,7 +38,38 @@ namespace BMI
                         vypocet = vaha / (vypocet);
                         vypocet = Math.Floor(vypocet);
                         Console.WriteLine();
-                        Console.Write($"Tvoje BMI je {vypocet}.");
+                        if (vypocet > 40)
+                        {
+                            Console.Write($"Tvoje BMI je tezkaobezita.");
+                        }
+                        else
+                        {
+                            if (vypocet > 30)
+                            {
+                                Console.Write($"Tvoje BMI je obezita.");
+                            }
+                            else
+                            {
+                                if (vypocet > 25)
+                                {
+                                    Console.Write($"Tvoje BMI je nadvaha.");
+                                }
+                                else
+                                {
+                                    if (vypocet > 20)
+                                    {
+                                        Console.Write($"Tvoje BMI je normalni.");
+                                    }
+                                    else
+                                    {
+                                        if (vypocet <= 20)
+                                        {
+                                            Console.Write($"Tvoje BMI je podvaha.");
+                                        }
+                                    }
+                                }
+                            }
+                        }
                     }
                 }
                 Console.ReadLine();
